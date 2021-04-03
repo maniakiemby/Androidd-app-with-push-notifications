a0520ce559b25602dd1cb0255097164a422bd704
Marcin Trzaska <Maniu190.94@o2.pl>
2021-04-03 16:51:22 +0200

    introduction of scrolling and correct display of tasks.

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.core.window import Window
 from kivy.properties import ObjectProperty
+from kivy.graphics import Color
 
 from database import ConnectionDb
 
@@ -30,37 +34,48 @@ def tasks_from_db():
     return tasks
 
 
-class ToDoApp(Screen):
+class ToDo(GridLayout, Screen):
     def __init__(self, **kwargs):
-        super(ToDoApp, self).__init__(**kwargs)
+        super(ToDo, self).__init__(**kwargs)
 
         self.cols = 1
 
-        self.inside = GridLayout()
-        self.inside.cols = 2
+        self.input_grid = GridLayout(cols=2, size_hint_y=None, height=100)
+        self.input_new_task = TextInput(multiline=False)
+        self.input_grid.add_widget(self.input_new_task)
+        self.input_submit = Button(text="Dodaj", font_size=20)
+        self.input_submit.bind(on_press=self.btn)
+        self.input_grid.add_widget(self.input_submit)
+        self.add_widget(self.input_grid)
 
-        self.task = TextInput(multiline=False)
-        self.add_widget(self.task)
-        self.submit = Button(text="Dodaj", font_size=20)
-        self.add_widget(self.submit)
+        self.all_tasks = tasks_from_db()
 
-        all_tasks = tasks_from_db()
+        self.tasks_grid = GridLayout(cols=1, spacing=2, size_hint_y=None)
+        self.tasks_grid.bind(minimum_height=self.tasks_grid.setter('height'))
 
-        for task in all_tasks:
-            self.inside.add_widget(Label(text=task))
-            self.submit = (Button(text="Zobacz", font_size=10))
-            self.submit.bind(on_press=self.vide)
-            self.inside.add_widget(self.submit)
+        for task in self.all_tasks:
+            button = Button(text=task, size_hint_y=None, font_size=18, background_color='purple', height=80)
+            # submit.bind(on_press=self.vide)
 
-        self.add_widget(self.inside)
+            self.tasks_grid.add_widget(button)
 
-    def btn(self):
-        print("zadanie:", self.new_task.text)
-        task_from_kv = self.new_task.text
+        grid_of_tasks = ScrollView(size_hint=(1, None), size=(Window.width, Window.height - 100))
+        grid_of_tasks.add_widget(self.tasks_grid)
+
+        self.add_widget(grid_of_tasks)
+
+    def btn(self, instance):
+        # print("zadanie:", self.new_task.text)
+        task_from_kv = self.input_new_task.text
         if task_from_kv is not "":
+            print("zadanie:", self.input_new_task.text)
             db = ConnectionDb(getenv('DB_NAME'))
             db.insert_task(table='Tasks', task=task_from_kv)
-            self.new_task.text = ''
+            self.input_new_task.text = ''
+
+            # self.add_widget(task_from_kv)
+        else:
+            print("nothink to add.")
 
     def vide(self, instance):
         sm.current = 'task_window'
@@ -79,20 +94,20 @@ class WindowManager(ScreenManager):
     pass
 
 
-# kv = Builder.load_file("my.kv")
+kv = Builder.load_file("MyApp.kv")
 
-sm = WindowManager()
+#sm = WindowManager()
 
-screens = [ToDoApp(name='tasks'), MainWindow(name='task_window')]
-for screen in screens:
-    sm.add_widget(screen)
+#screens = [ToDoApp(name='tasks'), MainWindow(name='task_window')]
+#for screen in screens:
+ #   sm.add_widget(screen)
 
-sm.current = 'tasks'
+#sm.current = 'tasks'
 
 
 class MyApp(App):
     def build(self):
-        return ToDoApp()
+        return ToDo()
 
 
 if __name__ == '__main__':
