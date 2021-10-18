2f3525a5583582562a6235af57e614967e9d408d
Marcin Trzaska <Maniu190.94@o2.pl>
2021-10-18 15:22:57 +0200

    first deploy app

---
+++ README.md
@@ -0,0 +1,7 @@
+to create environment:
+* first step, upgrade:
+python -m pip install --upgrade pip setuptools virtualenv
+* second step, create environment:
+python -m venv venv
+* activate environ
+* python -m pip install kivy[base] kivy_examples
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import getenv
 import re
 from functools import partial
+import time
 
 from dotenv import load_dotenv
 import kivy
@@ -10,7 +11,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, ScreenManagerException, \
+    FadeTransition, SlideTransition, NoTransition
 from kivy.uix.widget import Widget
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
@@ -44,12 +46,12 @@ class DatabaseValid:
         for i in task.splitlines():
             i = i.strip()
             i = re.sub("\s\s+", ' ', i)  # funkcja 'sub' zastępuje znalezione wyrażenia wybranym w drugim argumencie.
-            if i is not "":
+            if i != "":
                 new_string.append(i)
         string = "\n".join(new_string)
         if len(string) > 300:
             return 'long'
-        elif string is not '':
+        elif string != '':
             return string
         else:
             return False
@@ -62,8 +64,8 @@ class DatabaseValid:
 
         return tasks
 
-    def task_done(self, instance_id):
-        self.database_connect.mark_done('Tasks', instance_id)
+    def task_from_db(self, task_id):
+        ...
 
 
 class WrapButton(Button):
@@ -81,6 +83,101 @@ class WrapButton(Button):
         self._id = value
 
 
+class WrapButtonConfirm(WrapButton):
+    def __init__(self, **kwargs):
+        super(WrapButtonConfirm, self).__init__(**kwargs)
+
+        self.text = 'Wykonane'
+        self.background_normal = ''
+        self.font_size = 16
+        self.color = 'grey'
+        self.size_hint = (0.08, None)
+        self.size_hint_min_y = 20
+        self.size_hint_min_x = 20
+
+
+class WrapButtonNamed(WrapButton):
+    def __init__(self, **kwargs):
+        super(WrapButtonNamed, self).__init__(**kwargs)
+
+        self.background_normal = ''
+        self.font_size = 20
+        self.color = 'grey'
+        self.size_hint = (0.92, None)
+        self.size_hint_min_y = 20
+        self.size_hint_min_x = 20
+
+
+class GridButtons(GridLayout):
+    def __init__(self, **kwargs):
+        super(GridButtons, self).__init__(**kwargs)
+
+        self.bind(minimum_height=self.setter('height'))
+
+        self.all_tasks = self.receiving_tasks()
+
+        for task in self.all_tasks:
+            self.button_execute = WrapButtonConfirm(on_release=self.execute, _id=task[0])
+            self.add_widget(self.button_execute)
+
+            self.button_named = WrapButtonNamed(text=task[1], on_release=self.doorway, _id=task[0])
+            self.add_widget(self.button_named)
+
+    @staticmethod
+    def receiving_tasks():
+        db = DatabaseValid()
+        return db.tasks_from_db()
+
+    @staticmethod
+    def doorway(*instance):
+        instance_id = instance[0].id
+        MainWindow.current_id = instance_id
+        sm.transition.direction = 'left'
+        sm.current = 'task_window'
+
+    def execute(self, *instance):
+        instance_id = instance[0].id
+        db = DatabaseValid()
+        db.database_connect.mark_done('Tasks', instance_id)
+        self.remove_task(instance_id)
+
+    def remove_task(self, task_id):
+        for widget in self.walk(restrict=True):
+            try:
+                if widget.id == task_id:
+                    self.remove_widget(widget)
+            except:
+                pass
+
+    @staticmethod
+    def refresh(*args):
+        if sm.has_screen(name='tasks'):
+            sm.remove_widget(sm.get_screen('tasks'))
+            sm.add_widget(ToDoTasks(name='tasks'))
+
+        else:
+            raise ScreenManagerException(
+                "ScreenManager didn't found screen named 'tasks'"
+            )
+
+    @staticmethod
+    def refresh_new_task(*args):
+        if sm.has_screen(name='tasks'):
+            sm.transition = NoTransition()
+            sm.add_widget(ToDoTasks(name='new_tasks'))
+            sm.current = 'new_tasks'
+            sm.remove_widget(sm.get_screen('tasks'))
+            sm.add_widget(ToDoTasks(name='tasks'))
+            sm.current = 'tasks'
+            sm.remove_widget(sm.get_screen('new_tasks'))
+            sm.transition = SlideTransition()
+
+        else:
+            raise ScreenManagerException(
+                "ScreenManager didn't found screen named 'tasks'"
+            )
+
+
 class MainTasksScrollVIew(ScrollView):
     def __init__(self, **kwargs):
         super(MainTasksScrollVIew, self).__init__(**kwargs)
@@ -94,69 +191,13 @@ class MainTasksScrollVIew(ScrollView):
         return self
 
 
-class GridButtons(GridLayout):
-    def __init__(self, **kwargs):
-        super(GridButtons, self).__init__(**kwargs)
-
-        self.bind(minimum_height=self.setter('height'))
-
-        self.all_tasks = self.receiving_tasks()
-
-        for task in self.all_tasks:
-            self.button_execute = WrapButton(text='Wykonane', on_release=self.execute, _id=task[0],
-                                             background_normal='', font_size=16, color='grey', size_hint=(0.08, None),
-                                             size_hint_min_y=20, size_hint_min_x=20
-                                             )
-            self.add_widget(self.button_execute)
-
-            self.button = WrapButton(text=task[1], on_release=self.doorway, _id=task[0],
-                                     background_normal='', font_size=20, color='black', size_hint=(0.92, None),
-                                     size_hint_min_y=20, size_hint_min_x=20
-                                     )
-            self.add_widget(self.button)
-
-    @staticmethod
-    def receiving_tasks():
-        db = DatabaseValid()
-        return db.tasks_from_db()
-
-    @staticmethod
-    def doorway(*instance):
-        instance_id = instance[0].id
-        MainWindow.current_id = instance_id
-        sm.switch_to(screens[1], direction='left')
-        # sm.current = 'task_window'
-
-    # @staticmethod
-    def execute(self, *instance):
-        instance_id = instance[0].id
-        db = DatabaseValid()
-        db.task_done(instance_id)
-        for instance in self.walk():
-            print(instance)
-        # self.remove_widget(self.button)
-
-        # TODO Dodać funkcjonalność, która będzie rozróżniała zadania i będzie je można usunąć za pomocą polecenia
-        #  self.remove_widget(self.button)
-        #  np. spróbuj nadać nazwę każdemu przyciskowi np 'button1', 'button2', itd.
-
-    """def execute(self):
-        db = DatabaseValid()
-        db.task_done(instance_id)
-        
-    def remove_task(self, *instance):
-        if self.button(_id=task_id) in self.walk():
-            self.remove_widget(self.button(_id=task_id))
-        else:
-            print('Nie znalazłem widgetu')"""
-
-
 class ToDoTasks(Screen):
     def __init__(self, **kwargs):
         super(ToDoTasks, self).__init__(**kwargs)
         self.layout = GridLayout(cols=1, spacing=2)
 
         self.introduction_grid = GridLayout(cols=2)
+
         self.introduction_input = TextInput()
         self.introduction_grid.add_widget(self.introduction_input)
 
@@ -164,6 +205,7 @@ class ToDoTasks(Screen):
                                           size_hint_min_x=50, size_hint_min_y=50)
         self.introduction_submit.bind(on_press=self.btn)
         self.introduction_grid.add_widget(self.introduction_submit)
+
         self.layout.add_widget(self.introduction_grid)
 
         grid = MainTasksScrollVIew()
@@ -175,16 +217,17 @@ class ToDoTasks(Screen):
     def btn(self, instance):
         task_from_kv = self.introduction_input.text
         task = DatabaseValid.task_valid(task_from_kv)
-        if task is 'long':
+        if task == 'long':
             popup = Popup(title='Ostrzeżenie',
                           content=Label(text="Tekst jest zbyt rozległy, skróć go."),
                           size_hint=(None, None), size=(400, 400)
                           )
             popup.open()
-        elif task and task is not 'long':
+        elif task and task != 'long':
             db = DatabaseValid()
             db.database_connect.insert_task('Tasks', task)
             self.introduction_input.text = ''
+            GridButtons.refresh_new_task()
 
 
 class Task:
@@ -202,31 +245,78 @@ class Task:
         cls.task_content = task[0][1]
         cls.task_date_add = task[0][2]
         cls.task_date_of_performance = task[0][3]
-        print(
-            f'Task.receiving_content: {cls.task_id, cls.task_content, cls.task_date_add, cls.task_date_of_performance}'
-        )
 
 
 class MainWindow(Screen):
     current_id = None
     task_content = ObjectProperty(None)
     task_date_of_performance = ObjectProperty(None)
+    task = Task()
+
+    def button_back(self):
+        if self.task.task_content != self.task_content.text:
+            # show = PopupSavingChangedContent()
+            layout = FloatLayout()
+
+            popup = Popup(title='Odrzucić zmiany ?',
+                          content=layout,
+                          size_hint=(None, None), size=(400, 180), auto_dismiss=False,
+                          )
+
+            label = Label(text='Wprowadzone zmiany nie zostaną zapisane.', size_hint=(.6, .2),
+                          pos_hint={'x': .2, 'top': .88}
+                          )
+            button_cancel = Button(text='ANULUJ', size_hint=(.2, .1), pos_hint={'x': .1, 'y': .35})
+            button_cancel.bind(on_press=popup.dismiss)
+            button_discard = Button(text='ODRZUĆ', size_hint=(.2, .1), pos_hint={'x': .4, 'y': .35})
+            button_discard.bind(on_press=self.back)
+            button_discard.bind(on_press=popup.dismiss)
+            button_save = Button(text='ZAPISZ', size_hint=(.2, .1), pos_hint={'x': .7, 'y': .35})
+            button_save.bind(on_press=self.change_task_content)
+            button_save.bind(on_press=self.back)
+            button_save.bind(on_press=popup.dismiss)
+
+            layout.add_widget(label)
+            layout.add_widget(button_cancel)
+            layout.add_widget(button_discard)
+            layout.add_widget(button_save)
+
+            popup.open()
+        else:
+            self.back()
 
     @staticmethod
-    def button_back():
-        # sm.current = 'tasks'
-        sm.switch_to(screens[0], direction='right')
+    def back(*args):
+        sm.transition.direction = 'right'
+        sm.current = 'tasks'
 
     def on_enter(self, *args):
-        task = Task()
-        task.receiving_content(self.current_id)
+        self.task.receiving_content(self.current_id)
 
-        self.task_content.text = task.task_content
-        self.task_date_of_performance.text = task.task_date_of_performance
+        self.task_content.text = self.task.task_content
+        self.task_date_of_performance.text = self.task.task_date_of_performance
 
     def execute(self):
-        db = DatabaseValid()
-        return db.task_done(self.current_id)
+        db = DatabaseValid().database_connect
+        db.mark_done(table='Tasks', index=self.current_id)
+
+        GridButtons.refresh()
+        self.button_back()
+
+    def change_date_of_performance(self, *args):
+        popup = Popup(title='Edycja',
+                      content=Label(text=self.task_date_of_performance.text),
+                      size_hint=(None, 200), size=(400, 400), auto_dismiss=False
+                      )
+        popup.open()
+
+    def change_task_content(self, *args):
+        changed_content = self.task_content.text
+        task_validate = DatabaseValid.task_valid(changed_content)
+        if task_validate:
+            db = DatabaseValid()
+            db.database_connect.update_task_content(table='Tasks', index=self.current_id, task=task_validate)
+        GridButtons.refresh_new_task()
 
 
 class WindowManager(ScreenManager):
@@ -246,7 +336,7 @@ sm.current = 'tasks'
 
 class MyApp(App):
     def build(self):
-        # return ToDoTasks()
+        Window.clearcolor = (.376, .376, .376, 0)
         return sm
 
 
